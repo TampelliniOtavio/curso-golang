afe674227928adc75243d8ab25e2b600f1633381
Otavio Tampellini <tampelliniotavio@gmail.com>
2024-08-29 14:44:14 -0300

    Finalização de método Create

--- 14-projeto-envio-email/internal/domain/campaign/service.go
+++ 14-projeto-envio-email/internal/domain/campaign/service.go
@@ -1,11 +1,16 @@
 package campaign
 
-import "emailn/internal/contract"
+import (
+	"emailn/internal/contract"
+)
 
 type Service struct{
     Repository Repository
 }
 
-func (s *Service) Create(newCampaign contract.NewCampaign) error {
-    return nil
+func (s *Service) Create(newCampaign contract.NewCampaign) (string, error) {
+    campaign, _ := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+    s.Repository.Save(campaign)
+
+    return campaign.Id, nil
 }
