a30f6193311e582832b451325f998b7b321deb4e
Otavio Tampellini <tampelliniotavio@gmail.com>
2024-09-14 20:44:55 -0300

    Alterando retorno de erro para erro genérico

--- 14-projeto-envio-email/internal/domain/campaign/service_test.go
+++ 14-projeto-envio-email/internal/domain/campaign/service_test.go
@@ -2,6 +2,7 @@ package campaign
 
 import (
 	"emailn/internal/contract"
+	internalerrors "emailn/internal/internal-errors"
 	"errors"
 	"testing"
 
@@ -81,5 +82,5 @@ func Test_Create_ValidateRepositorySave(t *testing.T) {
 
     _, err := service.Create(newCampaign)
 
-    assert.Equal("error to save on database", err.Error())
+    assert.True(errors.Is(internalerrors.ErrInternal, err))
 }
