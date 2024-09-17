74fafebdc7e8d50196f889908c472bc0d03e7b78
Otavio Tampellini <tampelliniotavio@gmail.com>
2024-09-16 21:25:10 -0300

    Teste de HandlerError Sucesso

--- 14-projeto-envio-email/internal/endpoints/handler_error_test.go
+++ 14-projeto-envio-email/internal/endpoints/handler_error_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	internalerrors "emailn/internal/internal-errors"
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -42,4 +43,33 @@ func Test_HandlerError_when_endpoint_return_domain_error(t *testing.T) {
 
     assert.Equal(http.StatusBadRequest, res.Code)
     assert.Contains(res.Body.String(), "Domain Error")
+}
+
+func Test_HandlerError_when_endpoint_return_success(t *testing.T) {
+    assert := assert.New(t)
+    type bodyForTest struct{
+        Id int
+    }
+
+    body := bodyForTest{
+        Id: 2,
+    }
+
+    endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+        return body, 201, nil
+    }
+
+    handlerFunc := HandlerError(endpoint)
+    req, _ := http.NewRequest("GET", "/", nil)
+
+    res := httptest.NewRecorder()
+
+    handlerFunc.ServeHTTP(res, req)
+
+    assert.Equal(http.StatusCreated, res.Code)
+    objReturned := bodyForTest{}
+
+    json.Unmarshal(res.Body.Bytes(), &objReturned)
+
+    assert.Equal(body, objReturned)
 }
