2524d096a431d7ee4484ef8b5a86c64231992a6b
Otavio Tampellini <tampelliniotavio@gmail.com>
2024-09-16 21:21:00 -0300

    Teste de HandlerError Bad Request

--- 14-projeto-envio-email/internal/endpoints/handler_error_test.go
+++ 14-projeto-envio-email/internal/endpoints/handler_error_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	internalerrors "emailn/internal/internal-errors"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -25,3 +26,20 @@ func Test_HandlerError_when_endpoint_return_internal_error(t *testing.T) {
     assert.Equal(http.StatusInternalServerError, res.Code)
     assert.Contains(res.Body.String(), internalerrors.ErrInternal.Error())
 } 
+
+func Test_HandlerError_when_endpoint_return_domain_error(t *testing.T) {
+    assert := assert.New(t)
+    endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+        return nil, 0, errors.New("Domain Error")
+    }
+
+    handlerFunc := HandlerError(endpoint)
+    req, _ := http.NewRequest("GET", "/", nil)
+
+    res := httptest.NewRecorder()
+
+    handlerFunc.ServeHTTP(res, req)
+
+    assert.Equal(http.StatusBadRequest, res.Code)
+    assert.Contains(res.Body.String(), "Domain Error")
+}
