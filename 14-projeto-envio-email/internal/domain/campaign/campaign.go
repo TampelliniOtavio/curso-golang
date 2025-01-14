91e8a05edb04463fc0cd38d2226ea78cdb08c6e8
Otavio Tampellini <tampelliniotavio@gmail.com>
2025-01-14 14:21:17 -0300

    Envio de Email por goroutine

--- 14-projeto-envio-email/internal/domain/campaign/campaign.go
+++ 14-projeto-envio-email/internal/domain/campaign/campaign.go
@@ -8,60 +8,73 @@ import (
 )
 
 const (
-    Pending string = "Pending"
-    Started string = "Started"
-    Canceled string = "Canceled"
-    Deleted string = "Deleted"
-    Done    string = "Done"
+	Pending  string = "Pending"
+	Started  string = "Started"
+	Canceled string = "Canceled"
+	Deleted  string = "Deleted"
+	Failed   string = "Failed"
+	Done     string = "Done"
 )
 
 type Contact struct {
-    Id         string    `validate:"required" gorm:"size:50"`
-    Email      string    `validate:"email" gorm:"size:100"`
-    CampaignId string    `validate:"required" gorm:"size:50"`
+	Id         string `validate:"required" gorm:"size:50"`
+	Email      string `validate:"email" gorm:"size:100"`
+	CampaignId string `validate:"required" gorm:"size:50"`
 }
 
 type Campaign struct {
-    Id        string    `validate:"required" gorm:"size:50"`
-    Name      string    `validate:"min=5,max=24" gorm:"size:100"`
-    CreatedOn time.Time `validate:"required"`
-    Status    string    `validate:"required" gorm:"size:20"`
-    Content   string    `validate:"min=5,max=1024" gorm:"size:1024"`
-    Contacts  []Contact `validate:"min=1,dive"`
-    CreatedBy string    `validate:"required" gorm:"size:100"`
+	Id        string    `validate:"required" gorm:"size:50"`
+	Name      string    `validate:"min=5,max=24" gorm:"size:100"`
+	CreatedOn time.Time `validate:"required"`
+	Status    string    `validate:"required" gorm:"size:20"`
+	Content   string    `validate:"min=5,max=1024" gorm:"size:1024"`
+	Contacts  []Contact `validate:"min=1,dive"`
+	CreatedBy string    `validate:"required" gorm:"size:100"`
 }
 
 func (c *Campaign) Cancel() {
-    c.Status = Canceled
+	c.Status = Canceled
 }
 
 func (c *Campaign) Delete() {
-    c.Status = Deleted
+	c.Status = Deleted
+}
+
+func (c *Campaign) Fail() {
+	c.Status = Failed
+}
+
+func (c *Campaign) Done() {
+	c.Status = Done
+}
+
+func (c *Campaign) Started() {
+	c.Status = Started
 }
 
 func NewCampaign(name string, content string, emails []string, createdBy string) (*Campaign, error) {
-    campaignId := xid.New().String()
-    contacts := make([]Contact, len(emails))
-    for i, email := range emails {
-        contacts[i].Email = email
-        contacts[i].Id = xid.New().String()
-        contacts[i].CampaignId = campaignId
-    }
+	campaignId := xid.New().String()
+	contacts := make([]Contact, len(emails))
+	for i, email := range emails {
+		contacts[i].Email = email
+		contacts[i].Id = xid.New().String()
+		contacts[i].CampaignId = campaignId
+	}
 
-    campaign := &Campaign{
-        Id: campaignId,
-        Name: name,
-        Content: content,
-        CreatedOn: time.Now(),
-        Status: Pending,
-        Contacts: contacts,
-        CreatedBy: createdBy,
-    }
+	campaign := &Campaign{
+		Id:        campaignId,
+		Name:      name,
+		Content:   content,
+		CreatedOn: time.Now(),
+		Status:    Pending,
+		Contacts:  contacts,
+		CreatedBy: createdBy,
+	}
 
-    err := internalerrors.ValidateStruct(campaign)
-    if err != nil {
-        return nil, err
-    }
+	err := internalerrors.ValidateStruct(campaign)
+	if err != nil {
+		return nil, err
+	}
 
-    return campaign, nil
+	return campaign, nil
 }
--- 14-projeto-envio-email/internal/domain/campaign/service.go
+++ 14-projeto-envio-email/internal/domain/campaign/service.go
@@ -9,114 +9,120 @@ import (
 	"gorm.io/gorm"
 )
 
-type Service interface{
-    Create(newCampaign contract.NewCampaign) (string, error)
-    GetById(id string) (*contract.CampaignResponse, error)
-    Delete(id string) error
-    Start(id string) error
+type Service interface {
+	Create(newCampaign contract.NewCampaign) (string, error)
+	GetById(id string) (*contract.CampaignResponse, error)
+	Delete(id string) error
+	Start(id string) error
 }
 
-type ServiceImp struct{
-    Repository Repository
-    SendMail func(Campaign *Campaign) error
+type ServiceImp struct {
+	Repository Repository
+	SendMail   func(Campaign *Campaign) error
 }
 
 func (s *ServiceImp) Create(newCampaign contract.NewCampaign) (string, error) {
-    campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails, newCampaign.CreatedBy)
+	campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails, newCampaign.CreatedBy)
 
-    if err != nil {
-        return "", err
-    }
+	if err != nil {
+		return "", err
+	}
 
-    err = s.Repository.Create(campaign)
+	err = s.Repository.Create(campaign)
 
-    if err != nil {
-        if !errors.Is(err, gorm.ErrRecordNotFound) {
-            return "", internalerrors.ErrInternal
-        }
-        return "", err
-    }
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return "", internalerrors.ErrInternal
+		}
+		return "", err
+	}
 
-    return campaign.Id, err
+	return campaign.Id, err
 }
 
 func (s *ServiceImp) GetById(id string) (*contract.CampaignResponse, error) {
-    campaign, err := s.Repository.GetById(id)
-    if err != nil {
-        if !errors.Is(err, gorm.ErrRecordNotFound) {
-            return nil, internalerrors.ErrInternal
-        }
-        return nil, err
-    }
+	campaign, err := s.Repository.GetById(id)
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, internalerrors.ErrInternal
+		}
+		return nil, err
+	}
 
-    return &contract.CampaignResponse{
-        Id: campaign.Id,
-        Status: campaign.Status,
-        Content: campaign.Content,
-        AmountOfEmailsToSend: len(campaign.Contacts),
-        Name: campaign.Name,
-        CreatedBy: campaign.CreatedBy,
-    }, nil
+	return &contract.CampaignResponse{
+		Id:                   campaign.Id,
+		Status:               campaign.Status,
+		Content:              campaign.Content,
+		AmountOfEmailsToSend: len(campaign.Contacts),
+		Name:                 campaign.Name,
+		CreatedBy:            campaign.CreatedBy,
+	}, nil
 }
 
 func (s *ServiceImp) getAndValidateStatusIsPending(id string) (*Campaign, error) {
-    campaign, err := s.Repository.GetById(id)
+	campaign, err := s.Repository.GetById(id)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    if campaign.Status != Pending {
-        return nil, errors.New("Campaign status Invalid")
-    }
+	if campaign.Status != Pending {
+		return nil, errors.New("Campaign status Invalid")
+	}
 
-    return campaign, nil
+	return campaign, nil
 }
 
 func (s *ServiceImp) Delete(id string) error {
-    campaign, err := s.getAndValidateStatusIsPending(id)
+	campaign, err := s.getAndValidateStatusIsPending(id)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    var b Campaign
-    js, _ := json.Marshal(campaign)
-    json.Unmarshal(js, &b)
-    b.Delete()
+	var b Campaign
+	js, _ := json.Marshal(campaign)
+	json.Unmarshal(js, &b)
+	b.Delete()
 
-    err = s.Repository.Delete(&b)
+	err = s.Repository.Delete(&b)
 
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return internalerrors.ErrInternal
+		}
+		return err
+	}
 
-    if err != nil {
-        if !errors.Is(err, gorm.ErrRecordNotFound) {
-            return internalerrors.ErrInternal
-        }
-        return err
-    }
-
-    return nil
+	return nil
 }
 
+
 func (s *ServiceImp) Start(id string) error {
-    campaignSaved, err := s.getAndValidateStatusIsPending(id)
+	campaignSaved, err := s.getAndValidateStatusIsPending(id)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    err = s.SendMail(campaignSaved)
+	go func() {
+		err := s.SendMail(campaignSaved)
+		if err != nil {
+			campaignSaved.Fail()
+			return
+		}
 
-    if err != nil {
-        return internalerrors.ErrInternal
-    }
+		campaignSaved.Done()
 
-    campaignSaved.Status = Done
-    err = s.Repository.Update(campaignSaved)
+		s.Repository.Update(campaignSaved)
+	}()
 
-    if err != nil {
-        return internalerrors.ErrInternal
-    }
+	campaignSaved.Started()
+	err = s.Repository.Update(campaignSaved)
 
-    return nil
+	if err != nil {
+		return internalerrors.ErrInternal
+	}
+
+	return nil
 }
