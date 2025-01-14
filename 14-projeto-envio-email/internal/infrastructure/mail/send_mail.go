f506855a4ca7e95a38598c8ac4c98952e1e953d3
Otavio Tampellini <tampelliniotavio@gmail.com>
2025-01-14 14:20:54 -0300

    Formatação

--- 14-projeto-envio-email/internal/infrastructure/mail/send_mail.go
+++ 14-projeto-envio-email/internal/infrastructure/mail/send_mail.go
@@ -10,26 +10,26 @@ import (
 )
 
 func SendMail(campaign *campaign.Campaign) error {
-    m := gomail.NewMessage()
+	m := gomail.NewMessage()
 
-    port, err := strconv.ParseInt(os.Getenv("EMAIL_PORT"), 10, 32)
+	port, err := strconv.ParseInt(os.Getenv("EMAIL_PORT"), 10, 32)
 
-    if err != nil {
-        log.Fatal("Invalid Email Port", err.Error())
-    }
+	if err != nil {
+		log.Fatal("Invalid Email Port", err.Error())
+	}
 
-    d := gomail.NewDialer(os.Getenv("EMAIL_SMTP_PROVIDER"), int(port), os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASSWORD"))
+	d := gomail.NewDialer(os.Getenv("EMAIL_SMTP_PROVIDER"), int(port), os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASSWORD"))
 
-    var emails []string
+	var emails []string
 
-    for _, contact := range campaign.Contacts {
-        emails = append(emails, contact.Email)
-    }
+	for _, contact := range campaign.Contacts {
+		emails = append(emails, contact.Email)
+	}
 
-    m.SetHeader("From", os.Getenv("EMAIL_USER"))
-    m.SetHeader("To", emails...)
-    m.SetHeader("Subject", campaign.Name)
-    m.SetBody("text/html", campaign.Content)
+	m.SetHeader("From", os.Getenv("EMAIL_USER"))
+	m.SetHeader("To", emails...)
+	m.SetHeader("Subject", campaign.Name)
+	m.SetBody("text/html", campaign.Content)
 
-    return d.DialAndSend(m)
+	return d.DialAndSend(m)
 }
